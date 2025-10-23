66079a38af0d64c260b755e835c6426ae2263348
Ellothil <nreith4@gmail.com>
2025-10-23 20:36:38 +0200

    setup React and NestJS projects

---
+++ client/src/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-background text-foreground">
+      <h1>Start</h1>
+    </div>
+  );
+}
+
+export default App;
