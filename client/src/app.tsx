03b41779fc7b1043d5007177908ba2f71e368c22
Ellothil <nreith4@gmail.com>
2025-10-24 12:36:21 +0200

    basic collaboration with caret of other users

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,8 +1,11 @@
 import { EditorComponent } from "./components/editor";
 import { useTiptapEditor } from "./hooks/use-editor";
+import { useName } from "./hooks/use-name";
 
 function App() {
-  const editor = useTiptapEditor();
+  const { user } = useName();
+
+  const editor = useTiptapEditor(user);
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-background p-8 text-foreground">
